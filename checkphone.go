31b9e103ffea99e2c87d95f3fecbad2ddfcc8682
Milad Poshtdari <mposhtdari@gmail.com>
2022-03-14 07:04:57 +0330

    support german phone numbers

--- checkphone.go
+++ checkphone.go
@@ -5,12 +5,18 @@ import (
 )
 
 var (
-	IranPhoneChecker = regexp.MustCompile(`(?m)^(?:98|\+98|0098|0)?9[0-9]{9}$`)
+	IranPhoneChecker   = regexp.MustCompile(`(?m)^(?:98|\+98|0098|0)?9[0-9]{9}$`)
+	GermanPhoneChecker = regexp.MustCompile(`(\(?([\d \-\)\–\+\/\(]+){6,}\)?([ .\-–\/]?)([\d]+))`)
 )
 
 // Iran checking Iran Phone numbers.
 func Iran(n string) bool {
 	return IranPhoneChecker.MatchString(n)
+}
+
+// German checking Iran Phone numbers.
+func German(n string) bool {
+	return GermanPhoneChecker.MatchString(n)
 }
 
 // CheckIranPhone checking Iran Phone numbers.
